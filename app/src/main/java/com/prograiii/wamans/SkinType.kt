b3582a037e33940a6d178887432afe71df7abe57
Clau Orellana <clauorellana55@gmail.com>
2025-10-23 00:26:42 -0400

    pantalla de SkinType y viewBinding

--- app/src/main/java/com/prograiii/wamans/SkinType.kt
+++ app/src/main/java/com/prograiii/wamans/SkinType.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class InicioDeSesion : AppCompatActivity() {
+class SkinType : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_inicio_de_sesion)
+        setContentView(R.layout.activity_skin_type)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
